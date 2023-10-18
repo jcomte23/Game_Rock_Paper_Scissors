bf47aae384de53293db3321c961be54d276ccd1f
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-18 16:03:24 -0500

    the introduction menu was created

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+print()
+print(f"[ 🤖 Welcome to the game Rock, Paper, Scissors 🙋]")
+print()
+# quantity_rounds = int(input("      How many rounds do you want to play? ->"))
+quantity_rounds = 2
+print()
+print(" " * 11, "Let's go, start the game.")
+print()
