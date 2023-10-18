13febf216bfa89b4aebdee8ad8b635802b2e35cb
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-18 16:04:20 -0500

    the base menu was created

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+def run_game():
+    computer_wins = 0
+    user_wins = 0
+    rounds = 1
+
+    while True:
+        if rounds <= quantity_rounds:
+            print(" " * 8, '***' * 10)
+            print(" " * 20, 'Round ', rounds)
+            print(" " * 8, '***' * 10)
+            print(" " * 3, f">>> Choose an option (Enter the name) <<<")
+            rounds += 1
+
+            user_option, computer_option = choose_options()
+            print(user_option)
+            print(computer_option)
+        else:
+            break
+
+
 print()
 print(f"[ 🤖 Welcome to the game Rock, Paper, Scissors 🙋]")
 print()
